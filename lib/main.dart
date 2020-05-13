aa28a589817179b5e85b2c0a2026c46864c1d69b
Matthew Lees <matty5lees@gmail.com>
2020-05-13 14:41:24 +0200

    Added functionality to view all supplier in the database in separate tiles with questionable UI design, even by my standards

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bloomflutterapp/screens/authenticate/register.dart';
 import 'package:bloomflutterapp/screens/authenticate/sign_in.dart';
 import 'package:bloomflutterapp/screens/stock/add_stock.dart';
+import 'package:bloomflutterapp/screens/supplier/view_allsuppliers.dart';
 import 'package:bloomflutterapp/screens/wrapper.dart';
 import 'package:bloomflutterapp/services/auth.dart';
 import 'package:flutter/material.dart';
@@ -23,9 +24,10 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
 
         //Whichever screen Widget is defined in next line will be the screen shown when the app is run
-        home: AddStock(), //this will show the Add Stock screen
+        //home: AddStock(), //this will show the Add Stock screen
         //home: SignIn(), //this would show the sign in screen
         //home: Register(), //this would show the register screen
+        home: ViewAllSuppliers(),  //shows all the suppliers (basic 'contact book')
         // home: Wrapper(), //this is the wrapper that defines whether a user is authorised or not and sends them either to the landing page or the sign in screen
        // home: Home(),
       ),
