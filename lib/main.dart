6932383cf3f851b21aae2de5da2a48dc45076a96
Matthew Lees <matty5lees@gmail.com>
2020-05-13 09:22:39 +0200

    Added comments in main.dart to show how the initial screen is loaded when the application is run

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:bloomflutterapp/screens/authenticate/register.dart';
+import 'package:bloomflutterapp/screens/authenticate/sign_in.dart';
+import 'package:bloomflutterapp/screens/stock/add_stock.dart';
 import 'package:bloomflutterapp/screens/wrapper.dart';
 import 'package:bloomflutterapp/services/auth.dart';
 import 'package:flutter/material.dart';
@@ -10,13 +13,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of the application
+  //It defines which widget is run first through when the app is opened (so you can see a certain screen immediately)
   @override
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
       value: AuthService().user,
       child: MaterialApp(
-        home: Wrapper(),
+
+        //Whichever screen Widget is defined in next line will be the screen shown when the app is run
+        home: AddStock(), //this will show the Add Stock screen
+        //home: SignIn(), //this would show the sign in screen
+       // home: Register(), //this would show the register screen
+        // home: Wrapper(), //this is the wrapper that defines whether a user is authorised or not and sends them either to the landing page or the sign in screen
+
       ),
     );
   }
