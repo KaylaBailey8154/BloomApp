00e9c4b7706ebcfbc2ffbee926d56be41fd7ce6d
Matthew Lees <matty5lees@gmail.com>
2020-11-20 14:54:49 +0200

    Was being a silly chicken trying to do UI things that I don't understand

--- lib/screens/buyer/buyer_tile.dart
+++ lib/screens/buyer/buyer_tile.dart
@@ -95,7 +95,7 @@ class BuyerTile extends StatelessWidget {
                         height: 7,
                       ),
                       Text(
-                        message.substring(0,43),
+                        message,
                         style: TextStyle(
                           fontWeight: FontWeight.w500,
                           color: Colors.green[800],
